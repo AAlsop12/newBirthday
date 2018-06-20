8b79cdabe901ab83500c8e74b89a193eb801ed8b
AAslsop12 <aimee.johansen33@gmail.com>
2018-06-20 16:32:19 -0600

    installed moment and datePicker

---
+++ src/components/picker.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+
+class Picker extends Component {
+    render() {
+        return (
+            <div className="picker">
+                
+            </div>
+        )
+    }
+}
+
+export default Picker;
